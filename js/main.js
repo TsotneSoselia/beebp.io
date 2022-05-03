3e469e6d48a1925a6aa30b960a409706ab09d952
Tsotne77 <tsotnesoselia2@gmail.com>
2022-05-03 17:01:50 +0400

    add slider with javascript

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.reviews_slider').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        fade: true,
+        speed: 1000,
+    });
+  });
+
