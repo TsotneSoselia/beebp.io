06d37ffde1195a3eb6bbfaa23e2434bcd1f69d7d
Tsotne77 <tsotnesoselia2@gmail.com>
2022-05-09 23:39:37 +0400

    responsive/since footer

--- js/main.js
+++ js/main.js
@@ -1,11 +1,8 @@
 
 document.addEventListener("DOMContentLoaded", function(event){
-/* 
-    
-*/
  
-
-  let header = document.querySelector('.header'); //create ცვლადი. document.querySelectro('header') მივწვდი ამ კლასს html_ში
+  /********Header On Scroll *********/
+  let header = document.querySelector('.header'); //create ცვლადი. document.querySelectro('header') მივწვდი ამ კლასს html_ში(lova day6 13:00)
   let headerHeight = header.clientHeight; //ჰედერის სიმაღლე განვსაზღვრე
 
   window.onscroll = function(){ // window.onscroll = სქროლვისას
@@ -16,10 +13,19 @@ document.addEventListener("DOMContentLoaded", function(event){
   }
 }
 
+/******Burger Menu********/
+
+let headerBurger = document.querySelector('.header_burger');
+let menu = document.querySelector('.menu');
+let body = document.querySelector('body');
+headerBurger.onclick = function(){
+  headerBurger.classList.toggle('active');//toggle = ამატებს ერთხელ დაჭერაზე, მეორეზე თიშავს
+  menu.classList.toggle('active');
+  body.classList.toggle('no-scroll');
+}
 
 
-
-
+/******Reviews Slider********/
     $('.reviews_slider').slick({
         infinite: true,
         slidesToShow: 1,
